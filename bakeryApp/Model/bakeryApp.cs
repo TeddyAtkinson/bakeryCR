1df100bdb96021c5869af75e9379daada59983ff
Teddy Atkinson <tedddy.atkinson@gmail.com>
2022-12-09 13:02:14 -0800

    Add bakeryapp class structure

---
+++ bakeryApp/Model/bakeryApp.cs
@@ -0,0 +1,22 @@
+namespace Bakery 
+{
+    public class Bread
+    {
+        public int Loaves {get; set;}
+
+        public int loavesCalc()
+        {
+            //Buy 2 get 1 free. Every 3rd bread is free
+            //Cost of each 5$
+        }
+    }
+    public class Pastry
+    {
+        public int Pastries { get; set;}
+
+        public int pastiesCost()
+        {
+            //Buy 1 for 2$ or 3 for 5$
+        }
+    }
+}
